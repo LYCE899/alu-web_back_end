7950768b60483a26199707786aeb635db48e1dff
LYCE899 <a.coulibaly@alustudent.com>
2024-05-11 15:55:08 +0200

    commit

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,21 +2,15 @@
 
 
 """
-
     Basic annotations
 """
 
 
-
 def add(a: float, b: float) -> float:
     """
-
         A type-annotated function add that takes
-
         a float a and a float b as arguments
-
         and returns their sum as a float
     """
- 
-    jls_extract_var = a + b
-    return jls_extract_var
+
+    return a+b
