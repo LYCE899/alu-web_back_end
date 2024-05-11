730983776cfcc326eff2536f3c820136ebc21650
LYCE899 <a.coulibaly@alustudent.com>
2024-05-11 17:38:10 +0200

    new commit 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -6,5 +6,5 @@ from typing import Sequence, Iterable, List, Tuple
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ Element length """
     return [(i, len(i)) for i in lst]
- 
+
 # ...
