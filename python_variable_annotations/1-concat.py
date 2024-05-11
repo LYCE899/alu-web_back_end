d1e54302f0775fc0f7eaaa9fa52b5706af11eb53
LYCE899 <a.coulibaly@alustudent.com>
2024-05-11 16:57:01 +0200

    new commit 1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -7,6 +7,8 @@ Basic annotations: concat
 
 def concat(str1: str, str2: str) -> str:
     """
-    A function that returns a concatenated string
+    Concatenates two strings and returns the result
     """
     return str1 + str2
+
+# Ajout d'un saut de ligne à la fin du fichier
