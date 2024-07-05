9ef072b6eb951f818a1a886b8e6632d442e4c1d8
LYCE899 <a.coulibaly@alustudent.com>
2024-07-05 04:37:15 +0200

    commit

--- MySQL_Advanced/10-div.sql
+++
@@ -1,8 +0,0 @@
--- Create a function on MySQL
--- function divides (and returns) the first arument by the second one or zero if divisor is 0
-
-DELIMITER $$
-CREATE FUNCTION SafeDiv(a INT, b INT)
-       RETURNS FLOAT
-       RETURN (SELECT IF(b=0,0,a/b));
-$$
--- MySQL_Advanced/9-index_name_score.sql
+++
@@ -1,4 +0,0 @@
--- create a complex index
--- create an index on first letter of name and score too
-
-CREATE INDEX idx_name_first_score ON names (name(1), score);
